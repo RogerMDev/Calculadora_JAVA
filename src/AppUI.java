7a115c611e71ee87739b700016c1ac955cef44a5
Roger <rogermateom@gmail.com>
2025-05-09 17:55:31 +0200

    first commit

---
+++ src/AppUI.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class AppUI {
+    public static void main(String[] args) {
+        // Crear la finestra
+        JFrame ventana = new JFrame("Botó d'exemple");
+        ventana.setSize(300, 200);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLayout(new GridBagLayout()); 
+
+        // Crear el botó
+        JButton boto = new JButton("Fes click");
+        boto.setPreferredSize(new Dimension(100, 30));
+
+        // Afegir el botó
+        ventana.add(boto);
+
+        // Mostrar la finestra
+        ventana.setVisible(true);
+    }
+}
+
